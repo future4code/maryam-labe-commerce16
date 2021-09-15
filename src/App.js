c77b738513347cffbfebda8b1afb48c0be42e2d6
Matheus Guilherme Schade <matheus_schade@hotmail.com>
2021-09-15 18:08:25 -0300

    adicionado primeiro filtro

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
 
 export default class App extends React.Component {
 
@@ -9,15 +9,15 @@ export default class App extends React.Component {
 
   }
 
-  render(){
+  render() {
     return (
 
 
 
       <div className="App">
         <div className="filtro">Filtro</div>
-        <div className="meio">Meio</div> 
-        <div classname="carrinho">Carrinho</div> 
+        <div className="meio">Meio</div>
+        <div classname="carrinho">Carrinho</div>
 
 
 
